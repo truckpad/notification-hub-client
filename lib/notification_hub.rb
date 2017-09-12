c38e1c973a9afed6b22b11202e0f94783aed5287
Wagner Bigal <wagnerbigal@gmail.com>
2017-09-12 19:14:45 -0300

    Fixing path of files

--- lib/notification_hub.rb
+++ lib/notification_hub.rb
@@ -1,3 +1,9 @@
 require 'active_support/all'
-
-Dir['./lib/notification_hub/**/*.rb'].each { |file| require file }
+require 'notification_hub/notification'
+require 'notification_hub/channels/sqs'
+require 'notification_hub/channels/messages/base'
+require 'notification_hub/channels/messages/push_notification'
+require 'notification_hub/channels/messages/sms'
+require 'notification_hub/envelope/base'
+require 'notification_hub/envelope/fallback'
+require 'notification_hub/envelope/multichannel'
