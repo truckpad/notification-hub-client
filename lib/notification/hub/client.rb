5ddc9c5828b3c3c7ed81b0d457a742826a9ea51d
Wagner Bigal <wagnerbigal@gmail.com>
2017-09-12 12:47:32 -0300

    Initial setup

---
+++ lib/notification/hub/client.rb
@@ -0,0 +1,9 @@
+require "notification/hub/client/version"
+
+module Notification
+  module Hub
+    module Client
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/notification/hub/client/version.rb
@@ -0,0 +1,7 @@
+module Notification
+  module Hub
+    module Client
+      VERSION = "0.1.0"
+    end
+  end
+end
